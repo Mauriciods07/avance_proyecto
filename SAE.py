b5537cd301e228ae82cd550d23517a68602f7a90
Mauriciods07 <54260787+Mauriciods07@users.noreply.github.com>
2020-05-04 22:34:00 -0500

    codigo

--- SAE.py
+++ SAE.py
@@ -1,24 +1,36 @@
 #bibliotecas
 import nltk
 from modulo_sae import *
+import numpy
 
 
 #Cuerpo del programa      
 opcion = 0
 POS_TEXTO1, POS_TEXTO2 = Etiquetado_POS()
+valores = [0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
 
 while True: 
     print("Bienvenido al SAE")
-
-    print("¿Qué desea hacer?")
+    #...
+    opcion = int(input("¿Qué desea hacer?"))
+    
     if opcion is 1:
         Riqueza_lexica()
     elif opcion is 2:
         Dice_similarity()
     elif opcion is 3:
+        print("1. adjetivo")
+        print("2. sustantivo")
+        print("3. verbo")
+        print("4. adverbio")
+        print("5. determinante")
+        POS = int(input("¿Qué categoría gramatical necesitas?: "))
+        Contar_pos(POS)
+    elif opcion is 4:
         break
     else:
         print("Opción no válida")
+        
     print("¿Desea hacer otra prueba?")
     print("1. Sí")
     print("2. No")
--- modulo_sae.py
+++ modulo_sae.py
@@ -1,6 +1,14 @@
 #modulo en donde se encuentran lsa funciones del programa
 import request
 
+
+with open("Textos/texto01.txt", "r", encoding = 'utf8') as doc:
+    texto1 = doc.read()
+with open("Textos/texto02.txt", "r", encoding = 'utf8') as doc:
+    texto2 = doc.read()
+
+promedio = lambda numero1, numero2 : numero1 / numero2
+
 #Declarar funciones
 
 def Etiquetado_POS():
@@ -11,7 +19,7 @@ def Etiquetado_POS():
     r = requests.post(url, files=files, params=params)
     obj1 = r.json()
     
-    files = {'file': open('Textos/texto01.txt', 'rb')}
+    files = {'file': open('Textos/texto02.txt', 'rb')}
     params = {'outf': 'tagged', 'format': 'json'}
     url = "http://www.corpus.unam.mx/servicio-freeling/analyze.php"
     r = requests.post(url, files=files, params=params)
@@ -21,13 +29,9 @@ def Etiquetado_POS():
 #Riqueza_lexica se encarga de contar el número de palabras de cada texto. Con el número que salga de la razón, se puede hacer una estimación
 #Si el número es mayor a 1, los textos, posiblemente, pertenecen al mismo autor. Si el número es menor a 1, los textos no tienen una similitud muy amplia
 def Riqueza_lexica():
-    with open("Textos/texto01.txt", "r", encoding = 'utf8') as doc:
-        texto1 = doc.read()
-        dato1 =len(texto1) / len(set(texto1))
-    with open("Textos/texto02.txt", "r", encoding = 'utf8') as doc:
-        texto2 = doc.read()
-        dato2 = len(texto2) / len(set(texto2))
-    promedio = dato1 / dato2
+    dato1 =len(texto1) / len(set(texto1))
+    dato2 = len(texto2) / len(set(texto2))
+    prom = promedio(dato1, dato2)
     if promedio > 1:
         print("Los textos son semejantes")
     else:
@@ -62,3 +66,39 @@ def Dice_similarity():
     print("texto 2 ", len(WORDS2))
     print("acercamiento "+str((len(COMMON_WORDS))/(len(WORDS1) + len(WORDS2) - len(COMMON_WORDS))))
     return (2*len(COMMON_WORDS))/(len(WORDS1) + len(WORDS2))
+
+    #Esta función busca una categoría gramatical en específico por medio de la etiqueta obtenida con Freeling. Divide el total de veces que se repite dicha categoría y la divide entre el total de palabtas en el texto. Esto lo hace con cada archivo.
+#Finalmente, divide el promedio, nuevamente, entre sí. Si el número es cercano a 1, significa que los textos son similares.
+#Las cinco categorías gramaticales disponibles son adjetivos, sustantivos, verbos, adbverbios y determinantes.
+def Contar_pos(categoria):
+    if categoria == 1:
+        letra = 'A'
+    elif categoria == 2:
+        letra = 'N'
+    elif categoria == 3:
+        letra = 'V'
+    elif categoria == 4:
+        letra = 'R'
+    elif categoria == 5:
+        letra = 'D'
+        
+    categoria = 0
+    for oracion in POS_TEXTO1:
+        for palabra in oracion:
+            if palabra['tag'].startswith(letra):
+                categoria += 1
+    print(categoria)
+    categoria2 = 0
+    for oracion in POS_TEXTO2:
+        for palabra in oracion:
+            if palabra['tag'].startswith(letra):
+                categoria2 += 1
+    print(categoria2)
+    aprox = promedio(categoria, categoria2)
+    print("1 ", aprox)
+    aprox = round(aprox, 1)
+    print("2 ", aprox)
+    if aprox in valores:
+        print("Los textos son similares")
+    else:
+        print("Los textos no son similares")
